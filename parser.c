840260c2363bd5edfd00edccbdadd137f65d06c8
JoaoBarroso25 <73466271+JoaoBarroso25@users.noreply.github.com>
2021-05-21 10:13:53 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -104,7 +104,19 @@ void Oprations(STACK* s, char* token, int* AZ)
     if (strcmp(token, "+") == 0) op_plus(s);
 
     if (strcmp(token, "*") == 0) op_star(s);
-    
+
+
+    /*
+       {
+           Y = pop(s);
+           X = pop(s);
+           if (X.type == LONG && Y.type == LONG) XY.type = LONG;
+           else XY.type = DOUBLE;
+           XY.LONG = X.LONG * Y.LONG;
+           XY.DOUBLE = X.DOUBLE * Y.DOUBLE;
+           push(s, XY);
+       }
+   */
     if (strcmp(token, "-") == 0) {
         Y = pop(s);
         X = pop(s);
@@ -258,7 +270,7 @@ void Oprations(STACK* s, char* token, int* AZ)
         }
         push(s, X);
     }
-    if (strcmp(token, "e&") == 0) {   
+    if (strcmp(token, "e&") == 0) {   // verficar ainda
         Y = pop(s);
         X = pop(s);
         if (X.type == DOUBLE && Y.type == DOUBLE) {
@@ -274,7 +286,7 @@ void Oprations(STACK* s, char* token, int* AZ)
             }
         }
         else {
-            if (X.LONG == 0 || Y.LONG == 0) { 
+            if (X.LONG == 0 || Y.LONG == 0) { // esta linha
                 X.LONG = 0;
                 push(s, X);
             }
@@ -286,7 +298,7 @@ void Oprations(STACK* s, char* token, int* AZ)
             }
         }
     }
-    if (strcmp(token, "e|") == 0) {   
+    if (strcmp(token, "e|") == 0) {   // verficar - acho que ja esta bem
         X = pop(s);
         Y = pop(s);
         if (X.type == DOUBLE || Y.type == DOUBLE) {
@@ -308,7 +320,7 @@ void Oprations(STACK* s, char* token, int* AZ)
             else push(s, Y);
         }
     }
-    if (strcmp(token, "?") == 0) { 
+    if (strcmp(token, "?") == 0) { // verficar - x y z com ordem trocada
         X = pop(s);
         Y = pop(s);
         Z = pop(s);
@@ -332,7 +344,7 @@ void Oprations(STACK* s, char* token, int* AZ)
         X.type = CHAR;
         push(s, X);
     }
-    if (strcmp(token, "S") == 0) { 
+    if (strcmp(token, "S") == 0) { // VERIFICAR
         X.CHAR = ' ';
         X.type = CHAR;
         push(s, X);
@@ -659,4 +671,5 @@ void op_explosion(STACK *s)
         }
     }
 }
+
 
